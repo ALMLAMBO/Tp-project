4530d08bdbe9df148ee74f1a36f8ee38241f6d94
ALMLAMBO <aleksm007148@gmail.com>
2019-05-02 16:15:56 +0300

    Add displaying all pages

--- TpProject/Areas/Admin/Controllers/PagesController.cs
+++ TpProject/Areas/Admin/Controllers/PagesController.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TpProject.Models.Data;
+using TpProject.Models.ViewModels.Pages;
 
 namespace TpProject.Areas.Admin.Controllers {
     public class PagesController : Controller {
         // GET: Admin/Pages
         public ActionResult Index() {
-            return View();
+			List<PageVM> pagesList;
+
+			using (Db db = new Db()) {
+				pagesList = db.Pages
+					.ToArray()
+					.OrderBy(x => x.Sorting)
+					.Select(x => new PageVM(x))
+					.ToList();
+			}
+
+			return View(pagesList);
         }
     }
 }
