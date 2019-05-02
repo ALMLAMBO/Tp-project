5ca1da0fe48f955c98ebd2b57f10e35f02a475b6
ALMLAMBO <aleksm007148@gmail.com>
2019-05-03 00:02:00 +0300

    Add dto and view model for the sidebar

---
+++ TpProject/Models/ViewModels/Pages/SidebarVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TpProject.Models.Data;
+
+namespace TpProject.Models.ViewModels.Pages {
+	public class SidebarVM {
+		public SidebarVM() {
+
+		}
+
+		public SidebarVM(SidebarDTO row) {
+			Id = row.Id;
+			Body = row.Body;
+		}
+
+		public int Id { get; set; }
+		public string Body { get; set; }
+	}
+}
