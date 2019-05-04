a51ffabfb00d9eb9e14254b14d6ffbbf2454258e
ALMLAMBO <aleksm007148@gmail.com>
2019-05-04 19:22:14 +0300

    Add shop controller

---
+++ TpProject/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TpProject.Areas.Admin.Controllers {
+    public class ShopController : Controller {
+        // GET: Admin/Shop
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
