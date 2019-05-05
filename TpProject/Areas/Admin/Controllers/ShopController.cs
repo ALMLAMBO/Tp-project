7f65a0ddc2c4a9a75230245940f716a55f40d397
ALMLAMBO <aleksm007148@gmail.com>
2019-05-06 00:23:58 +0300

    Add get and post actions for adding new categories

--- TpProject/Areas/Admin/Controllers/ShopController.cs
+++ TpProject/Areas/Admin/Controllers/ShopController.cs
@@ -8,8 +8,8 @@ using TpProject.Models.ViewModels.Shop;
 
 namespace TpProject.Areas.Admin.Controllers {
     public class ShopController : Controller {
-        // GET: Admin/Shop/Categories
-        public ActionResult Categories() {
+        // GET: Admin/Shop/Index
+        public ActionResult Index() {
 			List<CategoryVM> categoriesVMList;
 			using (Db db = new Db()) {
 				categoriesVMList = db.Categories
@@ -20,5 +20,40 @@ namespace TpProject.Areas.Admin.Controllers {
 			}
             return View(categoriesVMList);
         }
+
+		[HttpGet]
+		public ActionResult AddNewCategory() {
+			return View();
+		}
+
+		// GET: Admin/Shop/AddNewCategory
+		[HttpPost]
+		public ActionResult AddNewCategory(CategoryVM model) {
+			if(!ModelState.IsValid) {
+				return View(model);
+			}
+
+			using (Db db = new Db()) {
+				CategoryDTO dto = new CategoryDTO();
+				dto.Name = model.Name;
+
+				if (db.Categories
+					.Any(x => x.Name == model.Name)) {
+					ModelState
+						.AddModelError("", "That category already exists.");
+
+					return View(model);
+				}
+
+				dto.Slug = model.Name.Replace(" ", "-").ToLower();
+				dto.Sorting = 100;
+
+				db.Categories.Add(dto);
+				db.SaveChanges();
+			}
+			TempData["SM"] = "You have added a new category!";
+
+			return RedirectToAction("AddNewCategory");
+		}
     }
 }
