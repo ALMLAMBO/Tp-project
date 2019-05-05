0eb1e6f12e35ec53797598a4ab6221fb1f357398
ALMLAMBO <aleksm007148@gmail.com>
2019-05-05 14:51:44 +0300

    Edit Categories action

--- TpProject/Areas/Admin/Controllers/ShopController.cs
+++ TpProject/Areas/Admin/Controllers/ShopController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TpProject.Models.Data;
+using TpProject.Models.ViewModels.Shop;
 
 namespace TpProject.Areas.Admin.Controllers {
     public class ShopController : Controller {
-        // GET: Admin/Shop
-        public ActionResult Index() {
-            return View();
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories() {
+			List<CategoryVM> categoriesVMList;
+			using (Db db = new Db()) {
+				categoriesVMList = db.Categories
+					.ToArray()
+					.OrderBy(x => x.Sorting)
+					.Select(x => new CategoryVM(x))
+					.ToList();
+			}
+            return View(categoriesVMList);
         }
     }
 }
