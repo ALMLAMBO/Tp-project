46284c0444a254895c3ccea4ed4db524f3ff5fa0
ALMLAMBO <aleksm007148@gmail.com>
2019-05-08 10:18:19 +0300

    Add dto and view model for the videos

---
+++ TpProject/Models/ViewModels/Shop/VideoVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TpProject.Models.Data;
+
+namespace TpProject.Models.ViewModels.Shop {
+	public class VideoVM {
+		public VideoVM() {
+
+		}
+
+		public VideoVM(VideoDTO row) {
+			Id = row.Id;
+			Name = row.Name;
+		}
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
