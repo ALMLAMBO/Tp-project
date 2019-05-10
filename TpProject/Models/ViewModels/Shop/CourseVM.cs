2b4e567be0d379977ee5a3e82aecc3791997abd7
ALMLAMBO <aleksm007148@gmail.com>
2019-05-10 09:41:28 +0300

    Add dto and view model for the course

---
+++ TpProject/Models/ViewModels/Shop/CourseVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TpProject.Models.Data;
+
+namespace TpProject.Models.ViewModels.Shop {
+	public class CourseVM {
+		public CourseVM() {
+
+		}
+
+		public CourseVM(CourseDTO row) {
+			Id = row.Id;
+			Name = row.Name;
+			Slug = row.Slug;
+			Description = row.Description;
+			Price = row.Price;
+			CategoryName = row.CategoryName;
+			CategoryId = row.CategoryId;
+			VideoId = row.VideoId;
+		}
+
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public string Slug { get; set; }
+		[Required]
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public string CategoryName { get; set; }
+		[Required]
+		public int CategoryId { get; set; }
+		public int VideoId { get; set; }
+
+		public IEnumerable<SelectListItem> Categories { get; set; }
+		public IEnumerable<string> VideoGallery { get; set; }
+	}
+}
