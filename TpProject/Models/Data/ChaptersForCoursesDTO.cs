3a2c06ade98c6311f57598d4a4c5ab81b3a0231e
ALMLAMBO <aleksm007148@gmail.com>
2019-05-21 23:15:34 +0300

    Add connection table for courses and all chapters for a course

---
+++ TpProject/Models/Data/ChaptersForCoursesDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TpProject.Models.Data {
+	[Table("tblChaptersForCourses")]
+	public class ChaptersForCoursesDTO {
+		[Key, Column(Order = 0)]
+		public int CourseId { get; set; }
+		[Key, Column(Order = 1)]
+		public int ChapterId { get; set; }
+
+		[ForeignKey("CourseId")]
+		public virtual CourseDTO Course { get; set; }
+		[ForeignKey("ChapterId")]
+		public virtual ChapterDTO Chapter { get; set; }
+	}
+}
