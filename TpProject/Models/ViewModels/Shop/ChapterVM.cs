a6f6fa3b7b6841d3cbfe40efff54e9ed86094def
ALMLAMBO <aleksm007148@gmail.com>
2019-05-21 23:13:39 +0300

    Add dto and view model for chapter

---
+++ TpProject/Models/ViewModels/Shop/ChapterVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TpProject.Models.Data;
+
+namespace TpProject.Models.ViewModels.Shop {
+	public class ChapterVM {
+		public ChapterVM() {
+
+		}
+
+		public ChapterVM(ChapterDTO row) {
+			Id = row.Id;
+			Name = row.Name;
+			Description = row.Description;
+			ChapterPath = row.ChapterPath;
+		}
+
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string ChapterPath { get; set; }
+	}
+}
