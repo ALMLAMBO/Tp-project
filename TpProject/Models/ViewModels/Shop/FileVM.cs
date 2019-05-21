b8f7970689bc55b3cf426145da32ed497555c1fb
ALMLAMBO <aleksm007148@gmail.com>
2019-05-21 23:14:28 +0300

    Add dto and view model for file

---
+++ TpProject/Models/ViewModels/Shop/FileVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TpProject.Models.Data;
+
+namespace TpProject.Models.ViewModels.Shop {
+	public class FileVM {
+		public FileVM() {
+
+		}
+
+		public FileVM(FileDTO row) {
+			Id = row.Id;
+			Name = row.Name;
+			FilePath = row.FilePath;
+		}
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string FilePath { get; set; }
+	}
+}
