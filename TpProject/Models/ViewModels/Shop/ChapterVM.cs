ff54723dbd20cf414c68f3a4c8622cb77b1eb377
ALMLAMBO <aleksm007148@gmail.com>
2019-05-22 13:57:40 +0300

    Add description to be required

--- TpProject/Models/ViewModels/Shop/ChapterVM.cs
+++ TpProject/Models/ViewModels/Shop/ChapterVM.cs
@@ -21,6 +21,7 @@ namespace TpProject.Models.ViewModels.Shop {
 		public int Id { get; set; }
 		[Required]
 		public string Name { get; set; }
+		[Required]
 		public string Description { get; set; }
 		public string ChapterPath { get; set; }
 	}
