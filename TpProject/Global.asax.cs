23ee03d80ccf0a1b208a7f5c7e1c0647bcb65ca9
ALMLAMBO <aleksm007148@gmail.com>
2019-06-04 17:44:53 +0300

    Add method for authentication request

--- TpProject/Global.asax.cs
+++ TpProject/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TpProject.Models.Data;
 
 namespace TpProject {
 	public class MvcApplication : System.Web.HttpApplication {
@@ -13,6 +15,30 @@ namespace TpProject {
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		protected void Application_AuthenticateRequest() {
+			if (User == null) {
+				return;
+			}
+
+			string username = Context.User.Identity.Name;
+			string[] roles = null;
+
+			using (Db db = new Db()) {
+				UserDTO dto = db.Users
+					.FirstOrDefault(x => x.Username == username);
+
+				roles = db.UserRoles
+					.Where(x => x.UserId == dto.Id)
+					.Select(x => x.Role.Name)
+					.ToArray();
+			}
+
+			IIdentity userIdentity = new GenericIdentity(username);
+			IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+			Context.User = newUserObj;
 		}
 	}
 }
