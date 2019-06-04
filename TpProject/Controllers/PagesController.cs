5ad8b3973d05f443ed1a6ab9fc5569948705e512
ALMLAMBO <aleksm007148@gmail.com>
2019-06-04 17:42:34 +0300

    Add pages controller not in the admin area

---
+++ TpProject/Controllers/PagesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TpProject.Models.Data;
+using TpProject.Models.ViewModels.Pages;
+
+namespace TpProject.Controllers {
+    public class PagesController : Controller {
+        // GET: Index/{page}
+        public ActionResult Index(string page = "") {
+			if (page == "") {
+				page = "home";
+			}
+
+			PageVM model;
+			PageDTO dto;
+			
+			using (Db db = new Db()) {
+				if (!db.Pages.Any(x => x.Slug.Equals(page))) {
+					return RedirectToAction("Index", new { page = "" });
+				}
+			}
+
+			using (Db db = new Db()) {
+				dto = db.Pages
+					.Where(x => x.Slug == page)
+					.FirstOrDefault();
+			}
+
+			ViewBag.PageTitle = dto.Title;
+
+			if (dto.HasSidebar == true) {
+				ViewBag.Sidebar = "Yes";
+			} 
+			else {
+				ViewBag.Sidebar = "No";
+			}
+
+			model = new PageVM(dto);
+
+			return View(model);
+        }
+    }
+}
