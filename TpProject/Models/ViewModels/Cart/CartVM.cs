b30a2deb615c5462829e28abd631071bc0f77cc3
ALMLAMBO <aleksm007148@gmail.com>
2019-06-10 23:17:14 +0300

    Add cart vm model

---
+++ TpProject/Models/ViewModels/Cart/CartVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TpProject.Models.ViewModels.Cart {
+	public class CartVM {
+		public int CourseId { get; set; }
+		public string CourseName { get; set; }
+		public int Quantity { get; set; }
+		public decimal Price { get; set; }
+		public decimal Total { get { return Quantity * Price; } }
+	}
+}
