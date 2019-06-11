749a31dea5e373583a17b059eb82216e01669ba2
ALMLAMBO <aleksm007148@gmail.com>
2019-06-11 16:57:23 +0300

    Format routes

--- TpProject/App_Start/RouteConfig.cs
+++ TpProject/App_Start/RouteConfig.cs
@@ -12,31 +12,63 @@ namespace TpProject {
 			routes.MapMvcAttributeRoutes();
 
 			routes.MapRoute(
-					name: "Account",
-					url: "Account/{action}/{id}",
-					defaults: new { controller = "Account", action = "Index",
-					id = UrlParameter.Optional}, new[] { "TpProject.Controllers" }
-				);
+				name: "Account", 
+				url: "Account/{action}/{id}", 
+				defaults: new { controller = "Account", action = "Index",
+					id = UrlParameter.Optional }, 
+				new[] { "TpProject.Controllers" }
+			);
 
 			routes.MapRoute(
-					name: "SidebarPartial",
-					url: "Pages/SidebarPartial",
-					defaults: new { controller = "Pages", action = "SidebarPartial" },
-					new[] { "TpProject.Controllers" }
-				);
+				name: "Cart", 
+				url: "Cart/{action}/{id}", 
+				defaults: new { controller = "Cart", action = "Index",
+					id = UrlParameter.Optional }, 
+				new[] { "TpProject.Controllers" }
+			);
+			
+			routes.MapRoute(
+				name: "Shop", 
+				url: "Shop/{action}/{name}", 
+				defaults: new { controller = "Shop", action = "Index",
+					name = UrlParameter.Optional }, 
+				new[] { "TpProject.Controllers" }
+			);
+			
+			routes.MapRoute(
+				name: "SidebarPartial", 
+				url: "Pages/SidebarPartial", 
+				defaults: new { controller = "Pages", action = "SidebarPartial" }, 
+				new[] { "TpProject.Controllers" }
+			);
 
 			routes.MapRoute(
 				name: "PagesMenuPartial", 
 				url: "Pages/PagesMenuPartial", 
 				defaults: new { controller = "Pages", action = "PagesMenuPartial" },
-				new[] { "CmsShoppingCart.Controllers" });
+				new[] { "TpProject.Controllers" }
+			);
 
-			//	routes.MapRoute(
-			//		name: "Default",
-			//		url: "{controller}/{action}/{id}",
-			//		defaults: new { controller = "Home",
-			//		action = "Index", id = UrlParameter.Optional }
-			//	);
+			routes.MapRoute(
+				name: "Pages", 
+				url: "{page}", 
+				defaults: new { controller = "Pages", action = "Index" }, 
+				new[] { "TpProject.Controllers" }
+			);
+			
+			routes.MapRoute(
+				name: "Default", 
+				url: "", 
+				defaults: new { controller = "Pages", action = "Index" }, 
+				new[] { "TpProject.Controllers" }
+			);
+
+			//routes.MapRoute(
+			//    name: "Default",
+			//    url: "{controller}/{action}/{id}",
+			//    defaults: new { controller = "Home", action = "Index",
+			//		id = UrlParameter.Optional }
+			//);
 		}
 	}
 }
